6f7f2f151861535fcfc413884790030b4fe5d207
Mukesh Prajapat <darwin.mukesh2022@gmail.com>
2023-06-09 09:55:42 +0530

    into jsx and react

--- src/index.js
+++ src/index.js
@@ -1,14 +1,54 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// ReactDOM --> virtualDOM
+
+// jsx --> javascript-xml  it is combination of javascript + html
+//jsx -->.... converted by babel into react.createlement
+{/* <>content</> --> jsx Fragment */}
+const name = "mukesh"
+// function by defination
+function abc(para){
+
+  return 7+9;
+}
+//function by expression  --> anonymous
+const printinfo = function (){
+  console.log("hello world");
+}
+
+//arrow function
+const xyz = () => {
+  console.log(new Date());
+}
+
+
+const obj = {
+  name:"yogesh",
+  age:25,
+  work:()=>{
+    console.log("dancing")
+  }
+}
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+<>
+<h1>{name} hello </h1>
+<h2>my name is yogesh </h2>
+<>{printinfo()}</>
+<>{abc()}</>
+<>{xyz()}</>
+<>{obj.work()}</>
+
+</>
+
+
 );
 
 // If you want to start measuring performance in your app, pass a function
